bdb81b1d716af11af1263983688a85f03eb643a5
hurjun1995 <hurjun1995@gmail.com>
2018-06-02 16:15:57 -0700

    loginForm made

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => (
-  <div>
-    <h1>Login Page</h1>
-    <Link to="/">Login</Link>
-  </div>
-);
+class LoginPage extends React.Component {
+  submit = data => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+
+        <LoginForm submit={this.submit} />
+        <Link to="/">Login</Link>
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
